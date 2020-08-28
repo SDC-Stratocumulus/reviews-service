3c54ce5983c5a056dab5d7c6c7aa9b10a55eff49
Anna Makagonova <makagonova.ann@gmail.com>
2020-08-27 23:27:07 -0400

    initialize db

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS reviews (
+  id SERIAL PRIMARY KEY,
+  product_id int,
+  rating int,
+  date timestamp,
+  summary VARCHAR,
+  body VARCHAR,
+  recommend boolean,
+  reported boolean,
+  reviewer_name VARCHAR,
+  reviewer_email VARCHAR,
+  response VARCHAR,
+  helpfulness int DEFAULT 0);
+
+CREATE TABLE IF NOT EXISTS photos (
+  id SERIAL PRIMARY KEY,
+  review_id int,
+  url VARCHAR);
+
+CREATE TABLE IF NOT EXISTS characteristics (
+  id SERIAL PRIMARY KEY,
+  product_id int,
+  name VARCHAR);
+
+CREATE TABLE IF NOT EXISTS characteristics_reviews (
+  id SERIAL PRIMARY KEY,
+  review_id int,
+  characteristics_id int,
+  value int);
+
+/*  Execute this file from the command line by typing:
+   psql -d reviews -f schema.sql
+ *  to create the database and the tables.*/
